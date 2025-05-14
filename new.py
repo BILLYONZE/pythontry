b2bc13e4f2a5f52550c39d79cc0f7c782f48860c
BILLYONZE <billjoy.kilai@gmail.com>
2025-05-14 14:47:03 +0300

    second trial

--- new.py
+++ new.py
@@ -1,8 +1,17 @@
-# This is a simple Python program that prints "Hello, World!"
+# Fibonacci Sequence Generator
 
-def main():
-    print("Hello World!")
+def fibonacci(n):
+    fib_sequence = [0, 1]  # Starting values for the Fibonacci sequence
+    while len(fib_sequence) < n:
+        next_number = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_number)
+    return fib_sequence
 
-if __name__ == "__main__":
-    main()
+# Input: Number of Fibonacci numbers you want
+n = int(input("Enter the number of Fibonacci numbers you want: "))
+
+# Output: Fibonacci sequence up to 'n' numbers
+fib_sequence = fibonacci(n)
+print(f"The first {n} numbers of the Fibonacci sequence are: {fib_sequence}")
+
     

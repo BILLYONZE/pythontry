b8bf32ce614c53742db5aaf1a90c28ccdb2deda4
BILLYONZE <billjoy.kilai@gmail.com>
2025-05-15 15:52:09 +0300

    index.js added

---
+++ index.js
@@ -0,0 +1,4 @@
+/*Here is where i start my JS code.
+ I will be using the express framework to create a simple web server that serves a static HTML file.
+ I will also use the fs module to read the HTML file from the filesystem.*/
+ console.log("Starting the server...");
